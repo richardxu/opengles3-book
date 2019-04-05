eac21e48f2e00f3a1fd94af4742f6177a6897c4c
richardxu <richard@macos>
2019-04-05 10:43:31 +0800

    fix the HelloTriangleNDK not run, now ok
    Change-Id: I920637d84b71dd753551867534e3856fac9f13c5

--- Chapter_2/HelloTriangleNDK/app/src/main/java/com/richard/hellotrianglendk/NativeActivity.java
+++ Chapter_2/HelloTriangleNDK/app/src/main/java/com/richard/hellotrianglendk/NativeActivity.java
@@ -1,30 +1,45 @@
 package com.richard.hellotrianglendk;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.TextView;
-
-public class NativeActivity extends AppCompatActivity {
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        //System.loadLibrary("native-lib");
-        System.loadLibrary("Hello_Triangle");
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_native);
-
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-       // tv.setText(stringFromJNI());
-    }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    //public native String stringFromJNI();
-}
+//
+//public class NativeActivity extends AppCompatActivity {
+//
+//    // Used to load the 'native-lib' library on application startup.
+//    static {
+//        //System.loadLibrary("native-lib");
+//        System.loadLibrary("Hello_Triangle");
+//    }
+//
+//    private final int CONTEXT_CLIENT_VERSION = 3;
+//    private GLSurfaceView mGLSurfaceView;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+////        setContentView(R.layout.activity_native);
+//
+//        // Example of a call to a native method
+////        TextView tv = (TextView) findViewById(R.id.sample_text);
+//       // tv.setText(stringFromJNI());
+//
+//        mGLSurfaceView = new GLSurfaceView(this);
+//        if(detectOpenGLES30())
+//        {
+//            mGLSurfaceView.setEGLContextClientVersion(CONTEXT_CLIENT_VERSION);
+//            //设置 Renderer 用于绘图
+//           // mGLSurfaceView.setRenderer(new );
+//        }
+//    }
+//
+//    private boolean detectOpenGLES30()
+//    {
+//        ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+//        ConfigurationInfo info = am.getDeviceConfigurationInfo();
+//        return (info.reqGlEsVersion >= 0x30000);
+//    }
+//
+//    /**
+//     * A native method that is implemented by the 'native-lib' native library,
+//     * which is packaged with this application.
+//     */
+//    //public native String stringFromJNI();
+//}
